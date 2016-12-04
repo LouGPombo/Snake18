bc561fa3ba4f9f94909362ee375b7c6bec6fd9d7
Lou <Lou@LOU-PC>
2016-12-04 18:30:04 +0100

    Agregar archivos de proyecto.

---
+++ Snake18/Game.cpp
@@ -0,0 +1,34 @@
+// Snake18.cpp: define el punto de entrada de la aplicación de consola.
+//
+
+#include "Game.h"
+
+
+rapidxml::xml_document<> doc;
+std::ifstream file("XMLFile.xml");
+std::stringstream buffer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+    return 0;
+}
+
---
+++ Snake18/Game.h
@@ -0,0 +1,16 @@
+// stdafx.h: archivo de inclusión de los archivos de inclusión estándar del sistema
+// o archivos de inclusión específicos de un proyecto utilizados frecuentemente,
+// pero rara vez modificados
+//
+
+#pragma once
+
+#include <stdio.h>
+#include <iostream>
+#include <time.h>
+#include <string>
+#include <chrono>
+#include "Game.cpp"
+
+
+// TODO: mencionar aquí los encabezados adicionales que el programa necesita
