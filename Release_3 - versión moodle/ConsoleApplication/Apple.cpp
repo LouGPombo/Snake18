9d7aa182c7845b1bd96667624dcb4e8aabda9545
Lourdes García <queiroga.pombo@gmail.com>
2017-01-11 23:54:22 +0100

    las dos versiones

---
+++ Release_3 - versión moodle/ConsoleApplication/Apple.cpp
@@ -0,0 +1,43 @@
+#include "Apple.h"
+
+
+
+Apple::Apple()
+{
+	_apple = 0;
+	_appleX = 0;
+	_appleY = 0;
+}
+
+
+Apple::~Apple()
+{
+}
+
+
+void Apple::setXApplePosition()
+{
+	srand(time(NULL));
+	_appleX = (int)rand() % 33 + 1;
+}
+
+void Apple::setYApplePosition()
+{
+	srand(time(NULL));
+	_appleY = (int)rand() % 18 + 1;
+}
+
+int Apple::getAppleSprite()
+{
+	return SPRITE_APPLE;
+}
+
+int Apple::getXApplePosition()
+{
+	return _appleX;
+}
+
+int Apple::getYApplePosition()
+{
+	return _appleY;
+}
